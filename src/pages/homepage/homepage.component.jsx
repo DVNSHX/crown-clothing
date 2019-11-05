5b62ed41595db6b1a9d6554b405959745b65471a
devanshdev <tyagidevanshofficial@gmail.com>
2019-11-05 17:52:20 +0530

    ok

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import './homepage.styles.scss'
-import Directory from '../../components/directory/directory.component' 
 
-const Homepage = () => {
-    return (
-        <div className='homepage'>
-            <Directory/>   
-        </div>
-    );
-};
+import Directory from '../../components/directory/directory.component';
 
-export default Homepage;
+import './homepage.styles.scss';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+);
+
+export default HomePage;
